b2f86aa20553a08612f7c65df2cffc867620a435
Christian Adamini <christian.adamini@invision.de>
2023-02-17 22:59:50 +0100

    Fix trailingSlash keyname

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'cadamini', // Usually your GitHub org/user name.
   projectName: 'docusaurus-test', // Usually your repo name.
-  railingSlash: false,
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
